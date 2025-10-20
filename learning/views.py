b01f2a4deb82c12b6903a838ecfb2dafa971ec0c
dinovatama <dionovatama11@gmail.com>
2025-10-20 20:26:02 +0700

    PROJECT-LMS FASE-2B

--- learning/views.py
+++ learning/views.py
@@ -2,6 +2,7 @@ from .models import CustomUser
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 
